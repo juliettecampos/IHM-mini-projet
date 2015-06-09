3d3f018c77212b9203c1e2745e936a92b95a063e
arondeaa <arondeaa@pc-dg-025-14.iut2.upmf-grenoble.fr>
2015-06-09 12:08:07 +0200

    On se la refait propre....

--- src/m2105_ihm/ui/FicheEvtUI.java
+++ src/m2105_ihm/ui/FicheEvtUI.java
@@ -3,33 +3,20 @@
  */
 package m2105_ihm.ui;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
-import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JList;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import m2105_ihm.nf.Mois;
 import m2105_ihm.nf.Contact;
 import m2105_ihm.nf.Evenement;
-import m2105_ihm.nf.Mois;
 
+/**
+ *
+ * @author IUT2
+ */
 public class FicheEvtUI extends javax.swing.JPanel {
-
-    private PlanningUI planning;
-    private JTextField intitule;
-    private JButton valider;
-    private JButton annuler;
-    private JButton ajouterParticipants;
-    private JButton retirerParticipants;
-    private JList listeParticipants;
-    private GridBagConstraints contrainte;
     
+    /*
+     * Attributs
+     */
+    private PlanningUI          planning;
     
     /**
      * Creates new form CarnetUI
@@ -50,16 +37,6 @@ public class FicheEvtUI extends javax.swing.JPanel {
         
         /** Projet : à compléter **/
         
-        if (valider != null){
-            valider.addActionListener(new ActionListener() {
-
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    planning.getSelectedEvt().setIntitule(intitule.getText());
-                }
-            });
-        }
-        
     }
     
     /**
@@ -67,50 +44,8 @@ public class FicheEvtUI extends javax.swing.JPanel {
      */    
     private void initUIComponents() {
         
-        this.setBorder(BorderFactory.createTitledBorder(planning.getSelectedDate()));
-        
-        if (planning.getSelectedEvt() != null){
-            //mise en place du layout
-            this.setLayout(new GridBagLayout());
-            
-            //élements
-            contrainte = new GridBagConstraints();
-            ajouterParticipants = new JButton("Ajouter participants");
-            retirerParticipants = new JButton("Retirer participants");
-            
-            //ajout des composants
-            contrainte.gridx = 0;
-            contrainte.gridy = 0;
-            contrainte.weightx = 1;
-            contrainte.anchor = GridBagConstraints.CENTER;
-            contrainte.weightx = 1.;
-            this.add(new JLabel("Intitulé : "), contrainte);
-
-            contrainte.gridx = 1;
-            contrainte.gridy = 0;
-            intitule = new JTextField(15);
-            this.add(intitule, contrainte);
-            
-            contrainte.gridx = 1;
-            contrainte.gridy = 3;
-            listeParticipants = new JList();
-            this.add(listeParticipants,contrainte);
-
-            contrainte.gridx = 1;
-            contrainte.gridy = 1;
-            valider = new JButton("Enregistrer");
-            contrainte.insets = new Insets(5, 0, 0, 5);
-            this.add(valider, contrainte);
-
-            contrainte.gridx = 1;
-            contrainte.gridy = 2;
-            annuler = new JButton("Annuler");
-            contrainte.ipadx = 25; 
-            this.add(annuler, contrainte);
-        } else{
-            this.add(new JLabel("Il n'y a pas d'évènements pour ce jour !"));
-        }
-
+        /** Projet : à compléter **/    
+  
     }
 
     /**
@@ -119,10 +54,9 @@ public class FicheEvtUI extends javax.swing.JPanel {
      * @return
      */        
     public boolean setValues(Evenement event) {
-        if (event == null) { return false; }
-        
-        intitule.setText(event.getIntitule());
-        listeParticipants.setListData(planning.getSelectedEvt().getParticipants());
+        if (event == null) { return false; }            
+                  
+        /** Projet : à compléter **/
             
         return false;
     }
@@ -135,8 +69,8 @@ public class FicheEvtUI extends javax.swing.JPanel {
     public boolean getValues(Evenement event) {
         
         if (event == null) { return false; }
-       
-        event.setIntitule(intitule.getText());
+        
+        /** Projet : à compléter **/
         
         return true;
     }
--- src/m2105_ihm/ui/PlanningUI.java
+++ src/m2105_ihm/ui/PlanningUI.java
@@ -4,51 +4,42 @@
 package m2105_ihm.ui;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.GridLayout;
+import java.awt.CardLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.Calendar;
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JLabel;
+
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.BorderFactory;
+import javax.swing.DefaultListModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.tree.TreeSelectionModel;
+
 import m2105_ihm.Controleur;
 import m2105_ihm.nf.Evenement;
-import m2105_ihm.nf.Mois;
 
 /**
  *
  * @author IUT2
  */
 public class PlanningUI extends JPanel {
-
-    private Controleur controleur;
-    private JPanel nord;
-    private JPanel centre;
-    private FicheEvtUI ficheEvt;
-    private JComboBox mois;
-    private JComboBox annee;
-    private ArrayList<JButton> jours;
-    private int selectedDay = Calendar.getInstance().getTime().getDay();
-    private ArrayList<Evenement> listeEvenements;
-    private Evenement selectedEvenement = null;
-
     /**
+     * Creates new form CarnetUI
+     */
+    private Controleur       controleur;
+    private FicheEvtUI       ficheEvt;
+
+    /** 
      * Constructeur : initialise les composants de l'IHM pour les événements
-     *
      * @param une instance du controleur
      */
     public PlanningUI(Controleur ctrl) {
         super();
-
+        
         this.controleur = ctrl;
         
-        this.listeEvenements = new ArrayList<>();
-        
-        initComponents();
+        initComponents();        
     }
 
     /**
@@ -57,300 +48,44 @@ public class PlanningUI extends JPanel {
     private void initComponents() {
         /*
          * Fiche événement
-         */
-        nord = new JPanel();
-        centre = new JPanel();
-        nord.setLayout(new GridLayout(1, 2));
-
-        mois = new JComboBox(Mois.values());
-        Mois currentMonth = null;
-        int nb = 0;
-        for (Mois m : Mois.values()) {
-            if (nb == Calendar.getInstance().get(Calendar.MONTH)) {
-                currentMonth = m;
-                break;
-            }
-            nb++;
-        }
-
-        mois.setSelectedItem(currentMonth);
-        mois.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent ae) {
-                setCalendar();
-                centre.updateUI();
-            }
-        });
-
-        annee = new JComboBox();
-        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
-        for (int i = 1900; i <= currentYear; i++) {
-            annee.addItem(i);
-        }
-
-        annee.setSelectedIndex(currentYear - 1900);
-        annee.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent ae) {
-                setCalendar();
-                centre.updateUI();
-            }
-        });
-
-        nord.add(mois);
-        nord.add(annee);
-
-        setCalendar();
-
+         */        
         ficheEvt = new FicheEvtUI(this);
-
-        this.setLayout(new BorderLayout());
-        this.add(nord, BorderLayout.NORTH);
-        this.add(centre, BorderLayout.CENTER);
-        this.add(ficheEvt, BorderLayout.EAST);
-    }
-
-    /**
-     * Ajoute un évènement à la liste des évènements
-     * et re-set le calendrier pour l'afficher avec le nouvel évènement
-     * 
-     * @param evt l'évènement à ajouter
-     * @return true ou false suivant si l'évènement a bien été ajouté ou non
-     */
-    public boolean ajouterEvt(Evenement evt) {
-        if (evt == null) {
-            return false;
-        }
-        
-        if (evt.getDateJour() == 1 && evt.getDateMois() == Mois.JANVIER && evt.getDateAnnee() == 1970){
-            evt.setDate(selectedDay, (Mois) mois.getSelectedItem(), annee.getSelectedIndex() + 1900);
-            selectedEvenement = evt;
-        }
-        
-        listeEvenements.add(evt);
-        setCalendar();
-        
-        return true;
-    }
-
-    /**
-     * Retire un évènement à la liste des évènements
-     * et re-set le calendrier pour l'afficher sans l'ancien évènement
-     * 
-     * @param evt l'évènement à retirer
-     * @return rue ou false suivant si l'évènement a bien été retiré ou non
-     */
-    public boolean retirerEvt(Evenement evt) {
-        if (evt == null) {
-            return false;
-        }
-
-        listeEvenements.remove(evt);
-        setCalendar();
-        
-        return false;
+     
+        this.add(new javax.swing.JLabel("Evenements"));     
     }
     
     /**
-     * Renvoie l'évènement sélectionné
-     * 
-     * @return l'évènement sélectionné l
+     * Ajoute une entrée dans la liste de événements
+     * @param title texte affiché dans la liste pour un contact
+     * @param Contact objet contact associé
      */
-    public Evenement getSelectedEvt() {        
-        return selectedEvenement;
+    public boolean ajouterEvt(Evenement evt) {
+        if (evt == null) { return false; }
+        
+        /** Projet à completer **/
+            
+        return true;
     }
-
+    
     /**
-     * Renvoie le jour de la semaine correspondant à une date
-     * 
-     * @param m le mois
-     * @param d le numéro du jour
-     * @param y l'année
-     * @return le jour de la semaine
-     */
-    public static int getDayOfDate(int m, int d, int y) {
-        int z = y - 1;
-        int D;
-        if (m >= 3) {
-            D = (((23 * m) / 9) + d + 4 + y + (y / 4) - (y / 100) + (y / 400) - 2) % 7;
-        } else {
-            D = (((23 * m) / 9) + d + 4 + y + (z / 4) - (z / 100) + (z / 400)) % 7;
-        }
-        return D;
+     * Retire une entrée dans l'arbre pour les contacts
+     * @param Contact contact à retirer
+     */    
+    public boolean retirerEvt(Evenement evt) {
+        if (evt == null) { return false; }
+        
+        /** Projet à completer **/
+            
+        return false;
     }
-
-    /**
-     * Renvoie le nombre de jour dans un mois
-     * 
-     * @param month le mois
-     * @param year l'année
-     * @return le nombre de jour dans le mois
+    
+    /*
+     * Retourne l'événement sélectionné
      */
-    public static int getNumberOfDayInMonth(int month, int year) {
-        Boolean leapYear = (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0));
-        int daysInMonth;
-        if (month == 4 || month == 6 || month == 9 || month == 11) {
-            daysInMonth = 30;
-        } else {
-            if (month == 2) {
-                daysInMonth = (leapYear) ? 29 : 28;
-            } else {
-                daysInMonth = 31;
-            }
-        }
-        return daysInMonth;
-    }
+    public Evenement getSelectedEvt() {    
+        
+        /** Projet à completer **/
 
-    /**
-     * Renvoie la date sélectionnée
-     * 
-     * @return la date sélectionnée
-     */
-    public String getSelectedDate() {
-        return selectedDay + "/" + (mois.getSelectedIndex() + 1) + "/" + (annee.getSelectedIndex() + 1900);
-    }
-
-    /**
-     * Mets en place le calendrier affiché sur l'interface
-     * et l'affiche
-     */
-    public void setCalendar() {
-        /**
-         * On efface tout
-         */
-        centre.removeAll();
-
-        /**
-         * On affiche les jours de la semaine
-         */
-        centre.add(new JLabel("Lundi"));
-        centre.add(new JLabel("Mardi"));
-        centre.add(new JLabel("Mercredi"));
-        centre.add(new JLabel("Jeudi"));
-        centre.add(new JLabel("Vendredi"));
-        centre.add(new JLabel("Samedi"));
-        centre.add(new JLabel("Dimanche"));
-
-        /**
-         * On récupère la date sélectionnée
-         */
-        int m = mois.getSelectedIndex() + 1;
-        int d = 1;
-        int y = annee.getSelectedIndex() + 1900;
-
-        /**
-         * On calcule le premier jour du mois
-         */
-        int D = getDayOfDate(m, d, y);
-
-        int nb = 0;
-        jours = new ArrayList<>();
-
-        int nombreJours = getNumberOfDayInMonth(m - 1, y);
-
-        /**
-         * On ajoute des boutons grisés à l'interface jusqu'au 
-         * premier jour du mois
-         */
-        for (int i = 1; i < D; i++) {
-            jours.add(new JButton(Integer.toString(i + 1 + nombreJours - D)));
-            jours.get(nb).setEnabled(false);
-            centre.add(jours.get(nb));
-            nb++;
-        }
-
-        nombreJours = getNumberOfDayInMonth(m, y);
-
-        int currentDay = Calendar.getInstance().getTime().getDay();
-        int currentMonth = Calendar.getInstance().get(Calendar.MONTH) + 1;
-        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
-
-        /**
-         * On ajoute les boutons pour chaque jour du mois
-         * qui permettent d'ajouter des évenements
-         */
-        for (int i = 1; i <= nombreJours; i++) {
-            jours.add(new JButton(Integer.toString(i)));
-            final int v = i;
-            Evenement ev = null;
-            
-            /**
-             * On parcours les évènements, et on le signale par un changement
-             * de couleur si il y en a un dans le mois
-             */
-            for (Evenement evenement : listeEvenements){
-                if (evenement.getDateJour() == i && (evenement.getDateMois().ordinal() + 1) == m && evenement.getDateAnnee() == y){
-                    jours.get(nb).setBackground(Color.red);
-                    System.out.println("Evenement le " + i + "/" + m + "/" + y + " : " + evenement.getIntitule());
-                    ev = evenement;
-                    break;
-                }
-            }
-            
-            final Evenement event = ev;
-            
-            jours.get(nb).addActionListener(new ActionListener() {
-
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    selectedDay = v;
-                    selectedEvenement = event;
-                    System.out.println(getSelectedDate());
-                    PlanningUI.this.remove(ficheEvt);
-                    ficheEvt = new FicheEvtUI(PlanningUI.this);
-                    ficheEvt.setValues(event);
-                    PlanningUI.this.add(ficheEvt, BorderLayout.EAST);
-                    ficheEvt.updateUI();
-                }
-            });
-            
-            if (i == currentDay && m == currentMonth && y == currentYear) {
-                jours.get(nb).setBackground(Color.yellow);
-            }
-            
-            if (ficheEvt != null){
-                this.remove(ficheEvt);
-            } 
-            ficheEvt = new FicheEvtUI(this);
-            ficheEvt.setValues(getSelectedEvt());
-            this.add(ficheEvt, BorderLayout.EAST);
-            ficheEvt.updateUI();
-            
-            centre.add(jours.get(nb));
-            nb++;
-        }
-
-        /**
-         * On ajoute ou non des jours permettant de faire un tableau
-         * complet de jour
-         */
-        if (nb > 35) {
-            int i = 1;
-            while (nb < 42) {
-                jours.add(new JButton(Integer.toString(i)));
-                jours.get(nb).setEnabled(false);
-                centre.add(jours.get(nb));
-                i++;
-                nb++;
-            }
-            centre.setLayout(new GridLayout(7, 7));
-        } else {
-            if (nb > 28) {
-                int i = 1;
-                while (nb < 35) {
-                    jours.add(new JButton(Integer.toString(i)));
-                    jours.get(nb).setEnabled(false);
-                    centre.add(jours.get(nb));
-                    i++;
-                    nb++;
-                }
-                centre.setLayout(new GridLayout(6, 7));
-            } else {
-                centre.setLayout(new GridLayout(5, 7));
-            }
-        }
-        centre.setBorder(BorderFactory.createTitledBorder("Calendrier"));
+        return null;
     }
 }
